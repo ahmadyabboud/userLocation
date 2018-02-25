4012c1002654d5f02567d67bfeb3a4440aa1a2fc
YOUGG <yougg@MacBook-Pro-de-YOUGG.local>
2018-02-25 19:39:35 +0100

    user Location swift 4
    get the user location using ios map kit on swift 4 , xcode 9

--- userLocation/ViewController.swift
+++ userLocation/ViewController.swift
@@ -7,12 +7,32 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
+class ViewController: UIViewController ,CLLocationManagerDelegate{
 
-class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var map: MKMapView!
+    
+    let manger = CLLocationManager()
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[0]
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(0.01, 0.01)
+        let myLocation:CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(myLocation, span)
+        map.setRegion(region, animated: true)
+        self.map.showsUserLocation = true
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        manger.delegate = self
+        manger.desiredAccuracy = kCLLocationAccuracyBest
+        manger.requestWhenInUseAuthorization()
+        manger.startUpdatingLocation()
     }
 
     override func didReceiveMemoryWarning() {
